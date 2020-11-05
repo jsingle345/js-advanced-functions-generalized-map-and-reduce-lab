acd5946e951773b8428be7ee1c2ef8d6583c20a8
jsingle345 <jsingle345@gmail.com>
2020-11-04 20:21:52 -0600

    commit tree c3b705d78af2664e3c5d02f2c190f51e57c6f0db

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+const map = (array, callback) => {
+    let result = []
+
+    for(let item in array){
+        result.push(callback(array[item]))
+    }
+    return result
+}
+
+const reduce = (array, callback, starter) => {
+    let beginner = (!!starter) ? starter : array[0]
+    let i = (!!starter) ? 0 : 1 
+
+    for (; i < array.length; i++){
+        beginner = callback(array[i], beginner)
+    }
+    return beginner
+}
